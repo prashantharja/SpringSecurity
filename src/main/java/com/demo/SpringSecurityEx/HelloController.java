df7884a4079d4c8da5d9a44eed4ac309cd7d871c
Prashanth Kumar Arja <parja@tractorsupply.com>
2026-02-15 18:28:52 +0530

    Initial commit for Spring Security project

---
+++ src/main/java/com/demo/SpringSecurityEx/HelloController.java
@@ -0,0 +1,15 @@
+package com.demo.SpringSecurityEx;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String greet(HttpServletRequest request) {
+		return "Welcome to Spring Security Example " + request.getSession().getId();
+	}
+}
---
+++ src/main/java/com/demo/SpringSecurityEx/StudentController.java
@@ -0,0 +1,30 @@
+package com.demo.SpringSecurityEx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+	List<Student> students = new ArrayList<>(List.of(
+			new Student(1, "Alice", 85),
+			new Student(2, "Bob", 90)
+		));
+
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+
+		return students;
+	}
+	
+	@PostMapping("/students")
+	public Student addStudent(@RequestBody Student student) {
+		students.add(student);
+		return student;
+	}
+}
